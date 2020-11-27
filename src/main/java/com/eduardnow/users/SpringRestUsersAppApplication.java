d2b4c084d6bccb637c6d4e9a927e538552fd4da4
Eduard Castro <eduard.cas07@gmail.com>
2020-11-27 15:54:25 -0500

    Initial commit

---
+++ src/main/java/com/eduardnow/users/SpringRestUsersAppApplication.java
@@ -0,0 +1,13 @@
+package com.eduardnow.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestUsersAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestUsersAppApplication.class, args);
+    }
+
+}
